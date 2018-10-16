bf39cf3d23dab1749fc47f0ededabd9f5f4c3cbe
campar <kralj.david.david@gmail.com>
2018-10-17 00:46:53 +0200

    added vue file

---
+++ neki fajl.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  	name: 'neki fajl', 
+  	components: {
+  	    // 'sample-name':require('./SampleFolder/SampleName.vue'),
+  	},
+	data () {
+	    return {
+
+		}
+	},
+	methods:{
+
+	}
+}
+</script>
+
+<style lang="css" scoped>
+	
+</style>		
